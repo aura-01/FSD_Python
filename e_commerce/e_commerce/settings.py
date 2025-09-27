26434dd483868d2b1017d9163a37ec506586d614
aura-01 <amruthachallagulla@gmail.com>
2025-09-27 09:17:28 +0530

    changes db

--- e_commerce/e_commerce/settings.py
+++ e_commerce/e_commerce/settings.py
@@ -91,14 +91,19 @@ WSGI_APPLICATION = "e_commerce.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
+  
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "e_commerce", # Database name
-        "USER": "root",      # Database user
-        "PASSWORD": "09092005@Amrutha",  # Database password
-        "HOST": "localhost", # Database host
-        "PORT": "3306",      # Database port
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
+    # "default": {
+    #     "ENGINE": "django.db.backends.mysql",
+    #     "NAME": "e_commerce", # Database name
+    #     "USER": "root",      # Database user
+    #     "PASSWORD": "09092005@Amrutha",  # Database password
+    #     "HOST": "localhost", # Database host
+    #     "PORT": "3306",      # Database port
+    # }
 }
 
 AUTH_USER_MODEL = 'website.AuthUser'  # Custom user model
