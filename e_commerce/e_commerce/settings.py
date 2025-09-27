cfe19c4b164adaa8b1740f05700f3e63e40efe2e
aura-01 <amruthachallagulla@gmail.com>
2025-09-27 09:27:48 +0530

    CORS origin

--- e_commerce/e_commerce/settings.py
+++ e_commerce/e_commerce/settings.py
@@ -151,8 +151,10 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",  # React development server    
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:5173",  # React development server    
+# ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_CREDENTIALS = True
